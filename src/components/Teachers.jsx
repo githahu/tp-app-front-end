1c4daf18c014d076c7ce412bff22f31bc4a9d23f
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-15 02:20:39 +0300

    created a table for teachers

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
-function Teachers() {
+const Teachers= ({teachers}) => {
+
+
   return (
-    <div>Teachers</div>
+    <div className="students-main-container">
+        <h1>Teachers</h1>
+        <div className="students-container">
+            <table>
+                <thead>
+                
+               
+                <th>Teacher ID</th>     
+                <th>Name</th>
+
+                <th>Grade </th>
+                
+                    
+                </thead>
+                <tbody>
+                    
+
+            {teachers.map((teacher) => {
+                return (
+                    <tr>
+                      
+                      <td>{teacher.id}</td>
+                        <td>{teacher.name}</td>
+                        <td className="center">{teacher.grade_id}</td>
+                       
+                    </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
+
+    
+  
 }
 
 export default Teachers
