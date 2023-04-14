914af43d85458bd348eef197aa4def71acae6fa4
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-14 21:27:56 +0300

    created component folder and included app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from 'react'
+import Navbar from './Navbar'
+import Students from './Students'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import Results from './Results'
+import Teachers from './Teachers'
+
+
 
 
 function App() {
 
+  // const [students, setStudents] = useState([]);
+     
+  // useEffect(() => {
+  //   fetch("http://localhost:9292/students")
+  //   .then((r) => r.json)
+  //   .then((data) => {
+  //     setStudents(data);
+  //   });
+  // }, []);
+
+
 
   return (
     <div className="App">
-     Home Page 
+     <Navbar  />
+     <Routes>
+     
+      <Route path="/students" element={<Students />} />
+      <Route path="/teachers" element={<Teachers />} />
+      <Route path="/results" element={<Results/>} /> 
+      <Route path="/" element={<Home />} />
+
+      
+     </Routes>
     </div>
   )
 }
