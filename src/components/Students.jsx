3b2cc1a54bc755f0aaea79d2b05f2023f4184bba
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-15 02:21:17 +0300

    created table for students

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
 
-const Students = () => {
+const Students = ({students}) => {
   return (
     <div className="students-main-container">
         <h1>Students</h1>
+        <div className="students-container">
+            <table>
+                <thead>
+                <th>Std ID</th>
+                <th>Name</th>
+                <th>Grade </th>
+                <th>Address</th>
+                    
+                </thead>
+                <tbody>
+                    
 
+            {students.map((student) => {
+                return (
+                    <tr>
+                        <td className="center">{student.id}</td>
+                        <td>{student.name}</td>
+                        <td className="center">{student.grade_id}</td>
+                        <td>{student.address}</td>
+                    </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
     </div>
   )
 }
